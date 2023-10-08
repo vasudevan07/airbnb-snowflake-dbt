4437cf91421b98ea489eaebfc33572c8b29189c8
vasudevan07 <saytovasu07@gmail.com>
2023-10-08 14:21:02 +0530

    added dbt project setup files and src models
    1) Added project setup files for dbt
    2) Added src models

---
+++ sql/setup_db_role_and_users_in_sf.sql
@@ -0,0 +1,35 @@
+-- Use an admin role
+USE ROLE ACCOUNTADMIN;
+
+-- Create the `transform` role and grant it to admin role
+CREATE ROLE IF NOT EXISTS transform;
+GRANT ROLE transform TO ROLE ACCOUNTADMIN;
+
+-- Create the default warehouse if necessary
+CREATE WAREHOUSE IF NOT EXISTS COMPUTE_WH;
+GRANT OPERATE ON WAREHOUSE COMPUTE_WH TO ROLE TRANSFORM;
+
+-- Create the `dbt` user and assign to role
+CREATE USER IF NOT EXISTS dbt
+PASSWORD = 'dbtPassword123'
+LOGIN_NAME = 'dbt'
+MUST_CHANGE_PASSWORD=FALSE
+DEFAULT_ROLE= 'transform'
+DEFAULT_WAREHOUSE = 'COMPUTE_WH'
+DEFAULT_NAMESPACE = 'AIRBNB.RAW'
+COMMENT = 'dbt user for data transformation';
+
+GRANT ROLE TRANSFORM TO USER dbt;
+
+
+-- Create database and schemas
+CREATE DATABASE IF NOT EXISTS AIRBNB;
+CREATE SCHEMA IF NOT EXISTS AIRBNB.RAW;
+
+-- Set up permissions to role `transform`
+GRANT ALL ON WAREHOUSE COMPUTE_WH TO ROLE transform; 
+GRANT ALL ON DATABASE AIRBNB to ROLE transform;
+GRANT ALL ON ALL SCHEMAS IN DATABASE AIRBNB to ROLE transform;
+GRANT ALL ON FUTURE SCHEMAS IN DATABASE AIRBNB to ROLE transform;
+GRANT ALL ON ALL TABLES IN SCHEMA AIRBNB.RAW to ROLE transform;
+GRANT ALL ON FUTURE TABLES IN SCHEMA AIRBNB.RAW to ROLE transform;
